25f3dc9dc67af6e71de22d7e63bec6f72d52f99b
Justin Lam <90014114+Zeltrut@users.noreply.github.com>
2024-10-06 11:49:10 -0700

    Update script.js
    hotfix to infinite loading screen

--- script.js
+++ script.js
@@ -2,8 +2,12 @@
 document.addEventListener("DOMContentLoaded", function () {
     const loadingScreen = document.getElementById('loading-screen');
     setTimeout(() => {
-        loadingScreen.classList.add('is-loaded');
-    }, 1000);
+        // Fade out and remove the loading screen
+        loadingScreen.style.opacity = 0;
+        setTimeout(() => {
+            loadingScreen.style.display = 'none';
+        }, 500); // Wait for fade-out transition to complete
+    }, 1000); // You can adjust this timeout if needed
 });
 
 // Sticky Navbar
